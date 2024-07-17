49808e77ec84c2191a9516f37482f46b29030e41
iqbalnova <macbookairm1@Meisy-Macbook.local>
2024-07-17 23:37:53 +0700

    update-gitignore [skip ci]

--- lib/utils/credential.dart
+++
@@ -1,6 +0,0 @@
-class Credential {
-  static const String baseUrl = 'https://api.themoviedb.org/3/';
-  static const String accessToken =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmYwZjQyNTNmMzE3ZmI5MjAzOTlkZmU0YjJlNzkwZSIsIm5iZiI6MTcyMTAyNjgzNi40ODA2NTgsInN1YiI6IjYxOTNkN2QzMDE3NTdmMDA2MGY2MzYzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h8JXT3v_PkAdyUodzzRnZ1_JK1iKzXM_n93PJ16oDiE';
-  static const String baseImage = 'https://image.tmdb.org/t/p/original';
-}
